c12ab8ea0063d7a3264f94170a5b767652e8c30e
sarvarmarimboyev <75431632+sarvarmarimboyev@users.noreply.github.com>
2023-04-13 22:30:25 +0500

    last

--- src/compnets/Pages/SellCar.jsx
+++ src/compnets/Pages/SellCar.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "../../styles/sell-car.css";
 import sellCar from "../../assets/images/sell-car.png";
 import TrackingCharts from "../../charts/TrackingCharts";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 const SellCar = () => {
+  const percentage = 55;
+  const  percentage2=66;
   return (
     <div className="sell__car">
       <div className="sell__car-wrapper">
@@ -43,9 +46,96 @@ const SellCar = () => {
                   <i class="ri-arrow-right-line"></i>
                 </span>
               </div>
-              <div className="box__02">
-                
+
+              <div className="circle__wrapper">
+                <div className="box__02">
+                  <CircularProgressbar
+                    value={percentage}
+                    text={`${percentage}%`}
+                    styles={buildStyles({
+                      pathColor: `#01d293`,
+                      textColor: "#fff",
+                      trailColor: "#0b0c28",
+                      backgroundColor: "#01d293",
+                      textSize: "18px",
+                    })}
+                  />
+                </div>
+                  <h4>Impression Share</h4>
               </div>
+                    <div className="box__03">
+                      <span className="model__spend-icon">
+                        <i class="ri-car-line"></i>
+                      </span>
+                      <h6 className="spend__amount">$1174</h6>
+                    <p className="spend__title">Model Spend</p>
+                    </div>
+                    <div className="box__04">
+                      <span className="model__spend-icon">
+                        <i class="ri-share-forward-line"></i>
+                      </span>
+                      <h6 className="spend__amount">$1174</h6>
+                    <p className="spend__title"> Spend per Unit turned</p>
+                    </div>
+                    <div className="box__05">
+                      <span className="model__spend-icon">
+                        <i class="ri-money-dollar-circle-line"></i>
+                      </span>
+                      <h6 className="spend__amount">$878</h6>
+                    <p className="spend__title">Model Spend</p>
+                    </div>
+            </div>
+
+            <div className="offer__item">
+              <div className="box__01">
+                <h3 className="client__name">Jon Doe </h3>
+                <h6 className="avg__price">
+                  $11,705 <span>Average price</span>
+                </h6>
+
+                <h6 className="market__price">Market averge is $16,605</h6>
+                <span className="arrow__key">
+                  <i class="ri-arrow-right-line"></i>
+                </span>
+              </div>
+
+              <div className="circle__wrapper">
+                <div className="box__02">
+                  <CircularProgressbar
+                    value={percentage2}
+                    text={`${percentage2}%`}
+                    styles={buildStyles({
+                      pathColor: `#01d293`,
+                      textColor: "#fff",
+                      trailColor: "#0b0c28",
+                      backgroundColor: "#01d293",
+                      textSize: "18px",
+                    })}
+                  />
+                </div>
+                  <h4>Impression Share</h4>
+              </div>
+                    <div className="box__03">
+                      <span className="model__spend-icon">
+                        <i class="ri-car-line"></i>
+                      </span>
+                      <h6 className="spend__amount">$1174</h6>
+                    <p className="spend__title">Model Spend</p>
+                    </div>
+                    <div className="box__04">
+                      <span className="model__spend-icon">
+                        <i class="ri-share-forward-line"></i>
+                      </span>
+                      <h6 className="spend__amount">$1174</h6>
+                    <p className="spend__title"> Spend per Unit turned</p>
+                    </div>
+                    <div className="box__05">
+                      <span className="model__spend-icon">
+                        <i class="ri-money-dollar-circle-line"></i>
+                      </span>
+                      <h6 className="spend__amount">$878</h6>
+                    <p className="spend__title">Model Spend</p>
+                    </div>
             </div>
           </div>
         </div>
--- src/compnets/Pages/Settings.jsx
+++ src/compnets/Pages/Settings.jsx
@@ -1,7 +1,66 @@
 import React from "react";
-
+import "../../styles/settings.css";
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="settings">
+      <div className="settings__wrapper">
+        <h2 className="settings__title">Settings page</h2>
+
+        <div className="settings__top">
+          <button className="settings__btn">My Details</button>
+          <button className="settings__btn active__btn">Profile</button>
+          <button className="settings__btn">Password</button>
+          <button className="settings__btn">Email</button>
+          <button className="settings__btn">Notification</button>
+        </div>
+        <div className="details__form">
+          <h2 className="profile__titel">Profile</h2>
+          <p className="profile__desc">Update your photo and personal details here</p>
+          <form>
+            
+            <div className="form__group">
+              <div>
+                <label> Live in</label>
+                <input type="text" placeholder=" Tashkent, Uzbekistan " />
+              </div>
+
+              <div>
+                <label> Street</label>
+                <input type="text" placeholder=" Sodiq Azimov " />
+              </div>
+            </div>
+
+
+
+            <div className="form__group">
+              <div>
+                <label> Email</label>
+                <input type="email" placeholder=" example@gmail.com " />
+              </div>
+
+              <div>
+                <label> Number</label>
+                <input type="number" placeholder=" +998 97 777 77 77 " />
+              </div>
+            </div>
+
+            <div className="form__group">
+              <div>
+                <label> Date of Birth</label>
+                <input type="date" placeholder=" dd/mm/yyyy " />
+              </div>
+
+              <div>
+                <label> Gender</label>
+                <input type="text" placeholder="Male" />
+              </div>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
